cb4494c5b5b29f559190f2a324dedc074d723353
bober <boberstarosta@gmail.com>
2018-10-06 17:09:39 +0200

    Preparing skeleton for node position calculation

--- railroad/network/scenery/basefixedtrack.py
+++ railroad/network/scenery/basefixedtrack.py
@@ -11,10 +11,9 @@ class BaseFixedTrack(BaseSceneryObject):
 
     def __init__(self, network, position, rel_node_positions, connection_indices, rotation=0):
         super().__init__(network, position, rotation)
-        # TODO: Transform relative node positions to get absolute positions.
-        absolute_node_positions = [p for p in rel_node_positions]
-        self.nodes = [Node(network, pos) for pos in absolute_node_positions]
+        self.nodes = [Node(network, Vec()) for _ in rel_node_positions]
         self.edges = [Edge(network, self.nodes[i0], self.nodes[i1], True) for i0, i1 in connection_indices]
+        self._update_node_positions()
 
     def delete(self):
         for edge in self.edges:
@@ -24,7 +23,11 @@ class BaseFixedTrack(BaseSceneryObject):
         super().delete()
 
     def on_position_changed(self, position):
-        pass
+        self._update_node_positions()
 
     def on_rotation_changed(self, rotation):
+        self._update_node_positions()
+
+    def _update_node_positions(self):
+        # TODO: Transform relative node positions to get absolute positions.
         pass
